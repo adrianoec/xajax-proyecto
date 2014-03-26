b1679b71994f324f7a14c8720d7f91aa591bf82b
ADRIANO <ADRIANO@idea-PC>
2014-03-26 06:00:55 -0500

    nuevo modelo bdd

--- funciones_xjx/factura_xajax.php
+++ funciones_xjx/factura_xajax.php
@@ -1,239 +1,232 @@
 <?php
 
 function validarForm($form, $opcion) {
-	$codigo_tipo_impuesto = strtoupper(trim($form['codigo_tipo_impuesto']));
+    $codigo_tipo_impuesto = strtoupper(trim($form['codigo_tipo_impuesto']));
 
-	$descripcion = strtoupper(trim($form['descripcion']));
+    $descripcion = strtoupper(trim($form['descripcion']));
 
-	$activo = strtoupper(trim($form['activo']));
+    $activo = strtoupper(trim($form['activo']));
 
-	$codigo_sri = strtoupper(trim($form['codigo_sri']));
+    $codigo_sri = strtoupper(trim($form['codigo_sri']));
 
-	global $enlace, $objPaginacion, $objComun;
+    global $enlace, $objPaginacion, $objComun;
 
-	$objResponse = new xajaxResponse();
+    $objResponse = new xajaxResponse();
 
-	$msg = "";
+    $msg = "";
 
-	if (strcasecmp($codigo_tipo_impuesto, '') == 0 or strcasecmp($codigo_tipo_impuesto, 'seleccione') == 0) {
-		$msg .= "\nINGRESE CODIGO TIPO IMPUESTO...";
-	}
+    if (strcasecmp($codigo_tipo_impuesto, '') == 0 or strcasecmp($codigo_tipo_impuesto, 'seleccione') == 0) {
+        $msg .= "\nINGRESE CODIGO TIPO IMPUESTO...";
+    }
 
-	if (strcasecmp($descripcion, '') == 0 or strcasecmp($descripcion, 'seleccione') == 0) {
-		$msg .= "\nINGRESE DESCRIPCION...";
-	}
+    if (strcasecmp($descripcion, '') == 0 or strcasecmp($descripcion, 'seleccione') == 0) {
+        $msg .= "\nINGRESE DESCRIPCION...";
+    }
 
-	if (strcasecmp($activo, '') == 0 or strcasecmp($activo, 'seleccione') == 0) {
-		$msg .= "\nSELECCIONE ACTIVO...";
-	}
+    if (strcasecmp($activo, '') == 0 or strcasecmp($activo, 'seleccione') == 0) {
+        $msg .= "\nSELECCIONE ACTIVO...";
+    }
 
-	if (strcasecmp($codigo_sri, '') == 0 or strcasecmp($codigo_sri, 'seleccione') == 0) {
-		$msg .= "\nINGRESE CODIGO SRI...";
-	}
+    if (strcasecmp($codigo_sri, '') == 0 or strcasecmp($codigo_sri, 'seleccione') == 0) {
+        $msg .= "\nINGRESE CODIGO SRI...";
+    }
 
-	if (strlen(trim($msg)) > 0) {
-		$objResponse -> alert($msg);
-		return $objResponse;
-	} else {
-		if ($opcion == 0) {// inserta
-			$objResponse -> call("xajax_ingresar", $form);
-		} else {// actualizar
-			$objResponse -> call("xajax_actualizar", $form);
-		}
-	}
-	return $objResponse;
-
+    if (strlen(trim($msg)) > 0) {
+        $objResponse->alert($msg);
+        return $objResponse;
+    } else {
+        if ($opcion == 0) {// inserta
+            $objResponse->call("xajax_ingresar", $form);
+        } else {// actualizar
+            $objResponse->call("xajax_actualizar", $form);
+        }
+    }
+    return $objResponse;
 }
 
 function ingresar($form) {
 
-	$codigo_tipo_impuesto = strtoupper(trim($form['codigo_tipo_impuesto']));
+    $codigo_tipo_impuesto = strtoupper(trim($form['codigo_tipo_impuesto']));
 
-	$descripcion = strtoupper(trim($form['descripcion']));
+    $descripcion = strtoupper(trim($form['descripcion']));
 
-	$activo = strtoupper(trim($form['activo']));
+    $activo = strtoupper(trim($form['activo']));
 
-	$codigo_sri = strtoupper(trim($form['codigo_sri']));
+    $codigo_sri = strtoupper(trim($form['codigo_sri']));
 
-	global $objPaginacion, $objComun;
+    global $objPaginacion, $objComun;
 
-	$objDB = new Database();
-	$objDB -> setParametrosBD(HOST, BASE, USER, PWD);
-	$objDB -> getConexion();
-	$objResponse = new xajaxResponse();
+    $objDB = new Database();
+    $objDB->setParametrosBD(HOST, BASE, USER, PWD);
+    $objDB->getConexion();
+    $objResponse = new xajaxResponse();
 
-	$sqlInsert = "insert into tipo_impuesto (descripcion,activo,codigo_sri) values";
+    $sqlInsert = "insert into tipo_impuesto (descripcion,activo,codigo_sri) values";
 
-	$sqlInsert .= "('$descripcion','$activo','$codigo_sri');";
+    $sqlInsert .= "('$descripcion','$activo','$codigo_sri');";
 
-	$rs = $objDB -> query($sqlInsert);
+    $rs = $objDB->query($sqlInsert);
 
-	$objResponse -> alert("Registrado...");
-	return $objResponse;
-
+    $objResponse->alert("Registrado...");
+    return $objResponse;
 }
 
 function actualizar($form) {
 
-	$codigo_tipo_impuesto = strtoupper(trim($form['codigo_tipo_impuesto']));
+    $codigo_tipo_impuesto = strtoupper(trim($form['codigo_tipo_impuesto']));
 
-	$descripcion = strtoupper(trim($form['descripcion']));
+    $descripcion = strtoupper(trim($form['descripcion']));
 
-	$activo = strtoupper(trim($form['activo']));
+    $activo = strtoupper(trim($form['activo']));
 
-	$codigo_sri = strtoupper(trim($form['codigo_sri']));
+    $codigo_sri = strtoupper(trim($form['codigo_sri']));
 
-	global $objPaginacion, $objComun;
+    global $objPaginacion, $objComun;
 
-	$objDB = new Database();
-	$objDB -> setParametrosBD(HOST, BASE, USER, PWD);
-	$objDB -> getConexion();
-	$objResponse = new xajaxResponse();
+    $objDB = new Database();
+    $objDB->setParametrosBD(HOST, BASE, USER, PWD);
+    $objDB->getConexion();
+    $objResponse = new xajaxResponse();
 
-	$sqlUpdate = "update  tipo_impuesto set  descripcion= '$descripcion'
+    $sqlUpdate = "update  tipo_impuesto set  descripcion= '$descripcion'
 , activo= '$activo'
 , codigo_sri= '$codigo_sri'
  where  codigo_tipo_impuesto= '$codigo_tipo_impuesto'
 ";
 
-	$rs = $objDB -> query($sqlUpdate);
+    $rs = $objDB->query($sqlUpdate);
 
-	$objResponse -> alert("Actualizado...");
-	return $objResponse;
-
+    $objResponse->alert("Actualizado...");
+    return $objResponse;
 }
 
 function confirmarEliminarForm($form) {
 
-	global $enlace, $objPaginacion, $objComun;
+    global $enlace, $objPaginacion, $objComun;
 
-	$objResponse = new xajaxResponse();
+    $objResponse = new xajaxResponse();
 
-	$objResponse -> confirmCommands(1, "Deseas eliminar el registro?");
-	$objResponse -> call("xajax_eliminar", $form);
-	return $objResponse;
-
+    $objResponse->confirmCommands(1, "Deseas eliminar el registro?");
+    $objResponse->call("xajax_eliminar", $form);
+    return $objResponse;
 }
 
 function eliminar($form) {
 
-	$codigo_tipo_impuesto = strtoupper(trim($form['codigo_tipo_impuesto']));
+    $codigo_tipo_impuesto = strtoupper(trim($form['codigo_tipo_impuesto']));
 
-	$descripcion = strtoupper(trim($form['descripcion']));
+    $descripcion = strtoupper(trim($form['descripcion']));
 
-	$activo = strtoupper(trim($form['activo']));
+    $activo = strtoupper(trim($form['activo']));
 
-	$codigo_sri = strtoupper(trim($form['codigo_sri']));
+    $codigo_sri = strtoupper(trim($form['codigo_sri']));
 
-	global $objPaginacion, $objComun;
+    global $objPaginacion, $objComun;
 
-	$objResponse = new xajaxResponse();
-	$objDB = new Database();
-	$objDB -> setParametrosBD(HOST, BASE, USER, PWD);
-	$objDB -> getConexion();
-	$objResponse = new xajaxResponse();
+    $objResponse = new xajaxResponse();
+    $objDB = new Database();
+    $objDB->setParametrosBD(HOST, BASE, USER, PWD);
+    $objDB->getConexion();
+    $objResponse = new xajaxResponse();
 
-	$sqlUpdate = "update  tipo_impuesto set activo=0 where  codigo_tipo_impuesto= '$codigo_tipo_impuesto'
+    $sqlUpdate = "update  tipo_impuesto set activo=0 where  codigo_tipo_impuesto= '$codigo_tipo_impuesto'
 ";
 
-	$rs = $objDB -> query($sqlUpdate);
+    $rs = $objDB->query($sqlUpdate);
 
-	$objResponse -> alert("Desactivado...");
-	return $objResponse;
-
+    $objResponse->alert("Desactivado...");
+    return $objResponse;
 }
 
 function limpiar($form) {
 
-	$objResponse = new xajaxResponse();
+    $objResponse = new xajaxResponse();
 
-	$objResponse -> assign("codigo_tipo_impuesto", "value", "");
-	$objResponse -> assign("descripcion", "value", "");
-	$objResponse -> assign("activo", "value", "");
-	$objResponse -> assign("codigo_sri", "value", "");
+    $objResponse->assign("codigo_tipo_impuesto", "value", "");
+    $objResponse->assign("descripcion", "value", "");
+    $objResponse->assign("activo", "value", "");
+    $objResponse->assign("codigo_sri", "value", "");
 
-	return $objResponse;
-
+    return $objResponse;
 }
 
 function seleccionar($id) {
 
-	$objResponse = new xajaxResponse();
+    $objResponse = new xajaxResponse();
 
-	$objDB = new Database();
-	$objDB -> setParametrosBD(HOST, BASE, USER, PWD);
-	$objDB -> getConexion();
+    $objDB = new Database();
+    $objDB->setParametrosBD(HOST, BASE, USER, PWD);
+    $objDB->getConexion();
 
-	$sql = " select *    from tipo_impuesto 
+    $sql = " select *    from tipo_impuesto 
     where  codigo_tipo_impuesto  = '$id' ";
 
-	$result = $objDB -> query($sql);
-	$numCols = $objDB -> getNumCols();
+    $result = $objDB->query($sql);
+    $numCols = $objDB->getNumCols();
 
-	$ln = $objDB -> fetch_array($result);
-	$codigo_tipo_impuesto = $ln[0];
-	$descripcion = $ln[1];
-	$activo = $ln[2];
-	$codigo_sri = $ln[3];
-	$objResponse -> assign("codigo_tipo_impuesto", "value", "$codigo_tipo_impuesto");
-	$objResponse -> assign("descripcion", "value", "$descripcion");
-	$objResponse -> assign("activo", "value", "$activo");
-	$objResponse -> assign("codigo_sri", "value", "$codigo_sri");
+    $ln = $objDB->fetch_array($result);
+    $codigo_tipo_impuesto = $ln[0];
+    $descripcion = $ln[1];
+    $activo = $ln[2];
+    $codigo_sri = $ln[3];
+    $objResponse->assign("codigo_tipo_impuesto", "value", "$codigo_tipo_impuesto");
+    $objResponse->assign("descripcion", "value", "$descripcion");
+    $objResponse->assign("activo", "value", "$activo");
+    $objResponse->assign("codigo_sri", "value", "$codigo_sri");
 
-	return $objResponse;
-
+    return $objResponse;
 }
 
 function consultar($form) {
-	$query = trim($form["txtConsulta"]);
-	$objResponse = new xajaxResponse();
+    $query = trim($form["txtConsulta"]);
+    $objResponse = new xajaxResponse();
 
-	$objDB = new Database();
-	$objDB -> setParametrosBD(HOST, BASE, USER, PWD);
-	$objDB -> getConexion();
+    $objDB = new Database();
+    $objDB->setParametrosBD(HOST, BASE, USER, PWD);
+    $objDB->getConexion();
 
-	$sql = " select *    from tipo_impuesto 
+    $sql = " select *    from tipo_impuesto 
     where  descripcion  like '%$query%' ";
 
-	$result = $objDB -> query($sql);
-	$numCols = $objDB -> getNumCols();
+    $result = $objDB->query($sql);
+    $numCols = $objDB->getNumCols();
 
-	$nuevo = "<img src='" . HOME . "imagenes/page_white_text.png'/>";
-	$nuevoLnk = " style='cursor:pointer' onclick = 'xajax_nuevo()' ";
+    $nuevo = "<img src='" . HOME . "imagenes/page_white_text.png'/>";
+    $nuevoLnk = " style='cursor:pointer' onclick = 'xajax_nuevo()' ";
 
-	$tabla = "<table border='0' align ='center' class='tablesorter' cellspacing='1'><thead><tr>";
-	$arrTi = $objDB -> field_name($result);
+    $tabla = "<table border='0' align ='center' class='tablesorter' cellspacing='1'><thead><tr>";
+    $arrTi = $objDB->field_name($result);
 
-	foreach ($arrTi as $ln) {
-		$campo = $ln;
-		$tabla .= "<th>$campo</th>";
-	}
-	$tabla .= " <th colspan='2' $nuevoLnk >$nuevo</th> </tr></thead><tbody>";
-	while ($ln = $objDB -> fetch_array($result)) {
-		$id = $ln[0];
-		$tb = "<tr>";
-		for ($i = 0; $i < $numCols; $i++) {
-			$dato = $ln[$i];
-			$tb .= "<td id = 'dv_$i'.'_$id'>$dato</td>";
-		}
-		$actualizar = "<img src='" . HOME . "imagenes/page_white_edit.png'/>";
-		$actalizarLnk = " style='cursor:pointer' onclick = 'xajax_seleccionar($id)' ";
-		$eliminar = "<img src='" . HOME . "imagenes/cross.png'/>";
-		$eliminarLnk = " style='cursor:pointer' onclick = 'xajax_confirmarEliminarForm($id)' ";
-		$tabla .= $tb . " <td $actalizarLnk >$actualizar</td><td $eliminarLnk >$eliminar</td>   </tr>";
-	}
-	$tabla .= "</tbody></table>";
-	$objResponse -> script('function loadTabla(){$("table").tablesorter({ widgets: [\'zebra\']});  }  $(function() {$("table") .tablesorter({ widgets: [\'zebra\']});  });');
-	$objResponse -> assign("dvRespuesta", "innerHTML", "$tabla");
-	return $objResponse;
+    foreach ($arrTi as $ln) {
+        $campo = $ln;
+        $tabla .= "<th>$campo</th>";
+    }
+    $tabla .= " <th colspan='2' $nuevoLnk >$nuevo</th> </tr></thead><tbody>";
+    while ($ln = $objDB->fetch_array($result)) {
+        $id = $ln[0];
+        $tb = "<tr>";
+        for ($i = 0; $i < $numCols; $i++) {
+            $dato = $ln[$i];
+            $tb .= "<td id = 'dv_$i'.'_$id'>$dato</td>";
+        }
+        $actualizar = "<img src='" . HOME . "imagenes/page_white_edit.png'/>";
+        $actalizarLnk = " style='cursor:pointer' onclick = 'xajax_seleccionar($id)' ";
+        $eliminar = "<img src='" . HOME . "imagenes/cross.png'/>";
+        $eliminarLnk = " style='cursor:pointer' onclick = 'xajax_confirmarEliminarForm($id)' ";
+        $tabla .= $tb . " <td $actalizarLnk >$actualizar</td><td $eliminarLnk >$eliminar</td>   </tr>";
+    }
+    $tabla .= "</tbody></table>";
+    $objResponse->script('function loadTabla(){$("table").tablesorter({ widgets: [\'zebra\']});  }  $(function() {$("table") .tablesorter({ widgets: [\'zebra\']});  });');
+    $objResponse->assign("dvRespuesta", "innerHTML", "$tabla");
+    return $objResponse;
 }
 
 function addrow($indice) {
-	global $enlace, $objPaginacion, $objComun;
-	$objResponse = new xajaxResponse();
-	$idx=$indice+2;
-	$indice = $indice + 1;
-	$row = "<table class='borde_detalle'>
+    global $enlace, $objPaginacion, $objComun;
+    $objResponse = new xajaxResponse();
+    $idx = $indice + 2;
+    $indice = $indice + 1;
+    $row = "<table class='borde_detalle'>
 	    <tr id='tr_$indice' class='borde_item'>
 		<td id='td_$indice" . "_1'  width='100px' class='borde_text'>		
 		<input type='text' class='borde_text' name='txtCP_$indice' id='txtCP_$indice' size='11' value='' />
@@ -270,57 +263,55 @@ function addrow($indice) {
 		</tr>
 						</table> <div id='dvDet$idx'></div>";
 
-$dev = "dvDet$indice";
-	$objResponse -> append("$dev", "innerHTML", $row);
-	$objResponse -> assign("txtIndice", "value", $indice);
+    $dev = "dvDet$indice";
+    $objResponse->append("$dev", "innerHTML", $row);
+    $objResponse->assign("txtIndice", "value", $indice);
 
-	return $objResponse;
-
+    return $objResponse;
 }
 
 function delrow($indice) {
 
-	global $enlace, $objPaginacion, $objComun;
+    global $enlace, $objPaginacion, $objComun;
 
-	$objResponse = new xajaxResponse();
-	$objResponse -> assign("tr_$indice", "innerHTML", "");
-	
-	return $objResponse;
+    $objResponse = new xajaxResponse();
+    $objResponse->assign("tr_$indice", "innerHTML", "");
 
+    return $objResponse;
 }
 
 function calculos($idx, $cant, $pu, $dsct) {
-	global $enlace, $objPaginacion, $objComun;
+    global $enlace, $objPaginacion, $objComun;
 
-	$objResponse = new xajaxResponse();
+    $objResponse = new xajaxResponse();
 
-	//$objResponse->alert("$idx, $cant, $pu, $dsct");
-	if ($cant == "") {
-		$cant = 0;
-	}
-	if ($pu == "") {
-		$pu = 0;
-	}
-	if ($dsct == "") {
-		$dsct = 0;
-	}
+    //$objResponse->alert("$idx, $cant, $pu, $dsct");
+    if ($cant == "") {
+        $cant = 0;
+    }
+    if ($pu == "") {
+        $pu = 0;
+    }
+    if ($dsct == "") {
+        $dsct = 0;
+    }
 
-	$cal = $cant * $pu - $dsct;
+    $cal = $cant * $pu - $dsct;
 
-	$objResponse -> assign("txtPT_$idx", "value", $cal);
-	return $objResponse;
+    $objResponse->assign("txtPT_$idx", "value", $cal);
+    return $objResponse;
 }
 
-$xajax -> register(XAJAX_FUNCTION, "addrow");
-$xajax -> register(XAJAX_FUNCTION, "delrow");
-$xajax -> register(XAJAX_FUNCTION, "calculos");
+$xajax->register(XAJAX_FUNCTION, "addrow");
+$xajax->register(XAJAX_FUNCTION, "delrow");
+$xajax->register(XAJAX_FUNCTION, "calculos");
 
-$xajax -> register(XAJAX_FUNCTION, "validarForm");
-$xajax -> register(XAJAX_FUNCTION, "ingresar");
-$xajax -> register(XAJAX_FUNCTION, "actualizar");
-$xajax -> register(XAJAX_FUNCTION, "eliminar");
-$xajax -> register(XAJAX_FUNCTION, "limpiar");
-$xajax -> register(XAJAX_FUNCTION, "confirmarEliminarForm");
-$xajax -> register(XAJAX_FUNCTION, "consultar");
-$xajax -> register(XAJAX_FUNCTION, "seleccionar");
+$xajax->register(XAJAX_FUNCTION, "validarForm");
+$xajax->register(XAJAX_FUNCTION, "ingresar");
+$xajax->register(XAJAX_FUNCTION, "actualizar");
+$xajax->register(XAJAX_FUNCTION, "eliminar");
+$xajax->register(XAJAX_FUNCTION, "limpiar");
+$xajax->register(XAJAX_FUNCTION, "confirmarEliminarForm");
+$xajax->register(XAJAX_FUNCTION, "consultar");
+$xajax->register(XAJAX_FUNCTION, "seleccionar");
 ?>
